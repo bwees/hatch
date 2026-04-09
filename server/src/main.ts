985fdeef154014c077a3021d70a90bbc0f532015
bwees <brandonwees@gmail.com>
2026-04-08 22:59:13 -0500

    feat: run database migrations on start

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,33 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import { IncomingMessage } from 'http';
 import { Duplex } from 'stream';
 import { AppModule } from './app.module';
 import { Go2RTCProxy } from './proxies/go2rtc.proxy';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
   const server = app.getHttpServer();
   const wsProxy = app.get(Go2RTCProxy);
+
+  logger.log('Running database migrations...');
+
+  migrate(app.get('db'), {
+    migrationsFolder: './src/db/migrations',
+  });
+
+  logger.log('Database migrations completed.');
 
   server.on('upgrade', (req: IncomingMessage, socket: Duplex, head: Buffer) => {
     if (req.url?.startsWith('/api/go2rtc')) {
       wsProxy.handleUpgrade(req, socket, head);
     }
   });
+
+  logger.log('WebSocket proxy for Go2RTC initialized');
 
   await app.listen(process.env.PORT ?? 3000);
 }
